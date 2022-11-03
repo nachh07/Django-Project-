e68e3f2e5b0b8c7815a0f5cbdf0399d02db7fa9f
nachh07 <39889859+nachh07@users.noreply.github.com>
2022-11-03 14:29:50 -0300

    Update models.py
    Se cambio el método ForeignKey por OneToOne para corregir el problema en el request

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class ExtensionUsuario(models.Model): 
-
-    # nos permite relacionar los objetos en la propiedad User con una pk 
-    # CASCADE = Si se borra el usuario que está relacionado la clase, borra el objeto
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
+class ExtensionUsuario(models.Model):
     
-    # para proyecto final descripción email etc
-    #descripcion = models.TextField()
+    avatar = models.ImageField(upload_to='avatares',null=True, blank=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
