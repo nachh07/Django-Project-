731586039f541c893edc5e15f5331080bf8795de
Nacho <ignaduplaa97@gmail.com>
2022-11-11 10:23:53 -0300

    Cambios finales en la descripción del about. Se quitó código que no estaba siendo utilizado.

--- Videogames/views.py
+++ Videogames/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render, redirect
 from Videogames.models import Videojuego
 from Videogames.forms import FormularioVideoJuego, FomularioBusqueda
 from datetime import datetime
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
@@ -15,29 +14,21 @@ def about(request):
     return render(request, 'layouts/about.html')
 
 def ver_catalogo(request): 
-
     cant_registros = Videojuego.objects.all().count()
     formulario = FomularioBusqueda()
-
     titulo = request.GET.get('titulo', None)
     if titulo: 
         videojuego = Videojuego.objects.filter(titulo__icontains = titulo)
     else: 
-        videojuego = Videojuego.objects.all()
-    
+        videojuego = Videojuego.objects.all()    
     return render(request, 'layouts/ver_catalogo.html', {'juego' : videojuego, 'cantidad' : cant_registros,  'form' : formulario})
 
 @login_required
 def registrar_videojuego(request):
-
-    if request.method == "POST":
-        
-        formulario = FormularioVideoJuego(request.POST)
-        
+    if request.method == "POST":       
+        formulario = FormularioVideoJuego(request.POST)        
         if formulario.is_valid():
-
             data = formulario.cleaned_data
-
             if not data['fecha_alta']:
                 fecha_alta = datetime.now()
             else: 
@@ -57,59 +48,15 @@ def registrar_videojuego(request):
             return render(request, 'layouts/registrar_videojuego.html', {'formulario' : formulario})
 
     formulario = FormularioVideoJuego()
-
     return render(request, 'layouts/registrar_videojuego.html', {'formulario' : formulario})
 
-# @login_required
-# def editar_videojuego(request, id): 
-    
-#     videojuego = Videojuego.objects.get(id=id)
-
-#     if request.method == "POST":
-#         formulario = FormularioVideoJuego(request.POST)
-
-#         if formulario.is_valid():
-#             data = formulario.cleaned_data
-#             if not data['fecha_alta']:
-#                 fecha_alta = datetime.now()
-#             else: 
-#                 fecha_alta = data['fecha_alta']
-
-#             videojuego = Videojuego(
-#                 titulo = data['titulo']
-#                 ,categoria = data['categoria']
-#                 ,precio = data['precio']
-#                 ,espacio_en_disco = data['espacio_en_disco']
-#                 ,fecha_alta = fecha_alta
-#             )
-            
-#             videojuego.save()
-#             redirect('ver_catalogo.html')
-#         else:
-#             return render(request, 'layouts/editar_videojuego.html', {'formulario' : formulario})
-
-#     formulario = FormularioVideoJuego(
-#         initial={
-#             'titulo' : videojuego.titulo,
-#             'categoria' : videojuego.categoria,
-#             'precio' : videojuego.precio,
-#             'espacio_en_disco' : videojuego.espacio_en_disco,
-#             'fecha_alta' : videojuego.fecha_alta
-#         }
-#     ) 
-    
-#     return render(request, 'layouts/editar_videojuego.html', {'formulario' : formulario, 'juego' : videojuego})
-
-
 class EditarVideojuego(LoginRequiredMixin, UpdateView):
-
     model = Videojuego
     success_url = '/videojuegos/catalogo'
     template_name = 'layouts/editar_videojuego.html'
     fields = ['titulo', 'categoria', 'precio', 'espacio_en_disco', 'fecha_alta']
 
 class BorrarVideojuego(LoginRequiredMixin, DeleteView): 
-
     model = Videojuego
     success_url = '/videojuegos/catalogo'
     template_name = 'layouts/borrar_videojuego.html'
